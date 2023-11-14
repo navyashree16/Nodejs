655a0ee686e91991f9719656aebe0f625fff25b3
navyashree16 <navyashreeh162001>
2023-11-14 15:59:48 +0530

    object

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const name = 'navya';
-let age = 23;
-const hasHobbies =true;
-
-// name = 'navyashree';
-age = 30;
-
-const summariseUser = (userName , userAge , userHasHobbies)=> {
-    return (
-        'Name is' +
-        userName +
-        ',age is' +
-        userAge +
-        'and the user has hobbies:' +
-        userHasHobbies
-    )
-}
+const person = {
+    name :'Navya',
+    age :22,
+    greet() {
+        console.log('Hi, I am ' + this.name)
+    }
+};
 
 
-//const add = (a,b) => a+b;
-const addOne = a => a+1;
 
-//console.log(add(1,2));
-console.log(addOne(1));
-
-console.log(summariseUser(name , age , hasHobbies));
+ person.greet()
